93ba1d5ecb3eedf2f5e4d7f7696378ee6185e88c
TC030 <terencechanbb@gmail.com>
2026-07-01 18:11:08 +0800

    feat(core): implement goal detection and ball reset mechanic

--- src/actors/Ball.cs
+++ src/actors/Ball.cs
@@ -3,18 +3,21 @@ using System;
 
 public partial class Ball : CharacterBody2D
 {
-	public const float Speed = 40.0f;
+	public const float Speed = 400.0f;
 	private Vector2 _velocity;
 	public override void _Ready()
 	{
-		_velocity = new Vector2(1.0f, 1.5f).Normalized() * Speed;
+		_velocity = new Vector2(-1.0f, 0.5f).Normalized() * Speed;
 	}
-
+	public void ResetBall()
+	{
+		Vector2 CenterOfScreen = GetViewportRect().Size / 2;
+		_velocity = new Vector2(1.0f, 0.5f).Normalized() * Speed;
+		GlobalPosition = CenterOfScreen;
+	}	
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = _velocity;
-
-		var collisionInfo = MoveAndCollide(velocity * (float)delta);
+		var collisionInfo = MoveAndCollide(_velocity * (float)delta);
 
 		if (collisionInfo != null)
 		{
---
+++ src/actors/World.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class World : Node2D
+{
+	[Export] private Ball _ball;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public void OnLeftGoalBodyEntered(Node2D body)
+	{
+		if (body is Ball)
+		{
+			_ball.ResetBall();
+			GD.Print("Left Goal Scored!");
+		}
+	}
+
+	public void OnRightGoalBodyEntered(Node2D body)
+	{
+		if (body is Ball)
+		{
+			_ball.ResetBall();
+			GD.Print("Right Goal Scored!");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
